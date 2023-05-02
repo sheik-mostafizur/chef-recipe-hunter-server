ef8573db0236f45a7206dd78458eeead764d9cbd
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-05-02 12:39:37 +0600

    add popular chines recipe data

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 const chinesChefData = require("./data/chines_chef.json");
+const popularChinesRecipe = require("./data/popular_chines_recipe.json");
 
 app.use(cors());
+
+app.use("/popular-chines-recipe", (req, res) => {
+  res.send(popularChinesRecipe);
+  res.end();
+});
 
 app.use("/chines-chef", (req, res) => {
   res.send(chinesChefData);
@@ -16,7 +22,6 @@ app.use("/", (req, res) => {
   res.send("<h1>Recipe Master API.</h1>");
   res.end();
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running at: http://localhost:${PORT}`);
 });
