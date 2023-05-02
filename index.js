56168b198c0c459e7b3b06c4166bb4627032bbba
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-05-02 11:42:43 +0600

    chines-chef route added

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
+const chinesChefData = require("./data/chines_chef.json");
+
 app.use(cors());
+
+app.use("/chines-chef", (req, res) => {
+  res.send(chinesChefData);
+  res.end();
+});
 
 app.use("/", (req, res) => {
   res.send("<h1>Recipe Master API.</h1>");
